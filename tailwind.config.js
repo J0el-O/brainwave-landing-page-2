36950b667a5ebabafffcfae8610c640686d0e55e
Joel <joelincode@gmail.com>
2024-04-04 01:14:56 +0100

    added widescreen and tallscreen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         dmsans:["DM Sans"]
-      }
+      },
+      screens: {
+        'widescreen':{ 'raw': '(min-aspect-ratio: 3/2)'}, 
+        'tallscreen':{ 'raw': '(min-aspect-ratio: 1/2)'}, 
+      } 
     },
   },
   plugins: [],
